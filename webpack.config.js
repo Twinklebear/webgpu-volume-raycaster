2a2eeca24a3f601b380aba8b1fafd1ba396386fc
Will Usher <will@willusher.io>
2021-09-18 10:00:35 -0700

    format

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,8 @@ module.exports = {
             }
         ]
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: "./index.html",
-        })
-    ],
+    plugins: [new HtmlWebpackPlugin({
+        template: "./index.html",
+    })],
 };
-
 
